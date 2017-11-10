e71dee16b76a7817e31daf22e223dd8194b19e3f
Stephanie McSpedon-Marx <s.mcspedon@gmail.com>
2017-11-10 21:08:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name){
+  drivers.push ('Ralph');
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift('Bob');
+}
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  const copyOfDrivers = [...drivers, 'Broom']
+  return copyOfDrivers;
+}
+
+function prependDriver(name) {
+  const copyOfDrivers = ['Broom', ...drivers];
+  return copyOfDrivers;
+}
+
+function removeLastDriver() {
+  const copyOfDrivers = drivers.slice(0, drivers.length -1)
+  return copyOfDrivers;
+}
+
+function removeFirstDriver() {
+  const copyOfDrivers = drivers.slice(1)
+  return copyOfDrivers;
+}
